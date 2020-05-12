2ad739733d9cbd611492bac863e9c8d73f9c453f
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-05-12 15:15:32 +0530

    Landing Page Wireframe

---
+++ pages/About.js
@@ -0,0 +1,37 @@
+import './app.module.css';
+function About() {
+    return (
+        <div className="body">
+            <div className="navbar">
+                <h1>Navbar Here</h1>
+        </div>
+            <div className="jumbotron">
+                <h1>Jumbotron Here</h1>
+            </div>
+            <div className="section">
+                <h1>Features Listing with CTAs... Here</h1>
+            </div>
+            <div className="section">
+                <h1>Left Right 1</h1>
+            </div>
+            <div className="section">
+                <h1>Left Right 2</h1>
+            </div>
+            <div className="section">
+                <h1>Testimonials Here</h1>
+            </div>
+            <div className="section-down">
+
+                <div className="membership">
+                    <h2>Memebership Here</h2>
+                </div>
+                <div className="footer">
+                    <h1>Footer Here</h1>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
