a7df9002efb6ee48dfa52f5c6e507601b152252e
Sandeep Dogra <sandeepdogra175@gmail.com>
2020-05-19 23:06:54 +0530

    footer partially completed

--- pages/Components/Footer/Footer.js
+++ pages/Components/Footer/Footer.js
@@ -1,11 +1,29 @@
 import "./footer.module.css";
 const Footer = () => {
-    return (
-        <div className = "footer">
-            <h1>
-                Footer Here!
-            </h1>
+  return (
+    <div className="main-footer">
+      <div >
+        <div className="row">
+          <div className="col">Mobile app</div>
+          <div className="col">Community</div>
+          <div className="col">Company</div>
+          <div className="col brand">Branding</div>
+          <div className="col">Help desk</div>
+          <div className="col">Contact</div>
+          <div className="col">Resources</div>
         </div>
-    )
-}
-export default Footer
+        <div className = "gola-container">
+            <div className = "Hello"/>
+            <div className = "Hello"/>
+            <div className = "Hello"/>
+            <div className = "Hello"/>
+        
+        </div>
+        <div className="row2">
+          <p className="col bla">&copy; Copyright Info and social media</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Footer;
