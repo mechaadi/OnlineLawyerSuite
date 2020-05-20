d867fe3f55eb2a956c408fed0b1c0246a5fe860f
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-05-20 12:01:30 +0530

    Register Screen Done

--- pages/Components/Register/Register.js
+++ pages/Components/Register/Register.js
@@ -1,9 +1,56 @@
 import './register.module.css';
-const Register = () => {
+import Link from 'next/link'
+const Login = () => {
     return (
-        <div className = "register">
-            <h1>That</h1>
+        <div>
+            <div className=" register flex-center">
+                <div className="app flex-center">
+                    <div className="header">
+                        Welcome Back.
+                </div>
+                    <div className="subheader">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                </div>
+                    <div className="box flex-center">
+
+
+                        <div className="form">
+                            <div className="box-title">
+                                Login
+                                </div>
+                        </div>
+                        <div className="formc">
+
+                            <div className="border">
+                                <input type="text" autoComplete={true} className="text-input" placeholder="Email Address" />
+                            </div>
+                            <div className="border">
+                                <input type="password" className="text-input" placeholder="Create Password" />
+                            </div>
+
+                        </div>
+                        <div className="buttons">
+                            <button className="a">Login</button>
+                            <button className="a">Register</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="membership">
+                    <div className="mem-left">
+                        <div className="mem-content">
+                            Membership
+                                 </div>
+                        <div className="mem-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </div>
+                    </div>
+                    <div className="mem-right">
+                        <Link href="/">
+                            <button className="a">Join Today</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
-export default Register
+export default Login
