99233a4c41f8aaeb2241166916594fbd04e83233
Sandeep Dogra <sandeepdogra175@gmail.com>
2020-05-23 19:59:10 +0530

    Footer fix

--- pages/Components/Footer/Footer.js
+++ pages/Components/Footer/Footer.js
@@ -1,29 +1,27 @@
 import footer from "./footer.module.css";
 const Footer = () => {
   return (
-    <div className={footer.main +' '+ footer.footer}>
-      <div>
-        <div className={footer.row}>
-          <div className={footer.col}>Mobile app</div>
-          <div className={footer.col}>Community</div>
-          <div className={footer.col}>Company</div>
-          <div className={footer.col + ' ' + footer.brand}>Branding</div>
-          <div className={footer.col}>Help desk</div>
-          <div className={footer.col}>Contact</div>
-          <div className={footer.col}>Resources</div>
-        </div>
-        <div className={footer.gola_container}>
-          <div className={footer.Hello} />
-          <div className={footer.Hello} />
-          <div className={footer.Hello} />
-          <div className={footer.Hello} />
-
-        </div>
-        <div className={footer.row2}>
-          <p className={footer.col + ' ' + footer.bla}>&copy; Copyright Info and social media</p>
-        </div>
+    <div>
+    <div className={footer.App}>
+      <div className={footer.left}>
+        <ul className={footer.list}>
+          <li className={footer.li}>Mobile App</li>
+          <li className={footer.li}>Community</li>
+          <li className={footer.li}>Company</li>
+        </ul>
       </div>
+      <div className={footer.brand}>Branding</div>
+      <div className={footer.left  + ' ' +  footer.right}>
+        <ul className={footer.list}>
+          <li className={footer.li}>Help Desk</li>
+          <li className={footer.li}>Contact</li>
+          <li className={footer.li}>Resources</li>
+        </ul>
+      </div>
+      {/* </div> */}
     </div>
+    <div className={footer.brand2}>Brand</div>
+  </div>
   );
 };
 export default Footer;
