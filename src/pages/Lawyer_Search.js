e9a6e1d24d1aecfb625e3b2b69644fc565cf81e2
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-05-31 17:59:22 +0530

    Added Search Bae

--- src/pages/Lawyer_Search.js
+++ src/pages/Lawyer_Search.js
@@ -3,6 +3,7 @@ import Nav from '../Components/Navbar/Navbar.js';
 import Chips from '../Components/Chips/Chips';
 import Card from '../Components/Card/Card';
 import Filter from '../Components/Filters/Filter';
+import Input from '../Components/Inputs/Input.js';
 const Lawyer_Search = () => {
     return (
         <div>
@@ -27,11 +28,18 @@ const Lawyer_Search = () => {
                             <div className={styles.laywer_filter_cheecked_items_container}>
                                 <Chips name="Adidas" />
                             </div>
-                            <Filter filter_title = "Supported Category" />
-                            <Filter filter_title = "Other Category"/>
+                            <Filter filter_title="Supported Category" />
+                            <Filter filter_title="Other Category" />
                         </div>
                     </div>
                     <div className={styles.lawyer_search_results_container}>
+                        <div className={styles.search_bar}>
+                            <Input placeholder="Search For Lawyers ..." />
+                            <div className={styles.search_icon_container}>
+                                <img src="https://cdn.discordapp.com/attachments/715197944202002584/716624918674145280/icons8-search-96.png" style={{ height: "90%", width: "90%", objectFit: "cover" }}/>
+                            </div>
+                        </div>
+
                         <div className={styles.lawyer_search_heading}>
                             Lawyers (0)
                         </div>
