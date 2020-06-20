409b7bf023411d992ecf16dc23ef6648ad0a0438
mechaadi <as808780@gmail.com>
2020-06-20 19:18:35 +0530

    added firebase configs

---
+++ prod/public/precache-manifest.68b258677cd096f9a9fd46584974eea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bdad6dac11a3d1c9e54d3f322e4529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e30faafbbd638a02745",
+    "url": "/static/css/main.92488308.chunk.css"
+  },
+  {
+    "revision": "387c6a9f314027e7dc17",
+    "url": "/static/js/2.b5a21a73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5a21a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e30faafbbd638a02745",
+    "url": "/static/js/main.695e415b.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "0affcf5098f7ae96a4788596269e2d8a",
+    "url": "/static/media/uplawwhite.0affcf50.png"
+  }
+]);
