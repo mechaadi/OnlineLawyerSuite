f9d5642fb27b3c1b86f4eb706a1e0b933da1525a
mechaadi <as808780@gmail.com>
2020-06-22 02:10:21 +0530

    build

---
+++ frontend/build/precache-manifest.8c6a85707986871780d69eac7aea5d55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c38762449d4a1dd5d9747b3c684e5d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ed6a1b4d6009c881ca",
+    "url": "/static/css/main.6b4b3987.chunk.css"
+  },
+  {
+    "revision": "13233dfb61f364715351",
+    "url": "/static/js/2.563bd0e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.563bd0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ed6a1b4d6009c881ca",
+    "url": "/static/js/main.bf782705.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "b74f09f04ecaed462f58b5da1a52d066",
+    "url": "/static/media/uplawwhite.b74f09f0.png"
+  }
+]);
